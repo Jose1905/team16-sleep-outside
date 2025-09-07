7107eaa367dac0932d7c3b3886b1fd9424af0deb
josedvega <davarela231@gmail.com>
2025-09-06 22:30:20 -0600

    Individual activity 1 complete

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,15 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  let cartItems = getLocalStorage('so-cart');
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
   cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  setLocalStorage('so-cart', cartItems);
 }
 
 // add to cart button event handler
@@ -17,5 +20,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
