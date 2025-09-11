128edf8b95cae0fd0822b5fb6c027f1e31dd8e2b
josedvega <davarela231@gmail.com>
2025-09-10 21:02:02 -0600

    Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,7 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import { updateCartCount } from "./main.js";
+import { getParam, qs } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 // Update cart count on page load
 document.addEventListener("DOMContentLoaded", () => {
@@ -8,18 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-  updateCartCount(".cart-count"); // Update cart count after adding product
-}
+console.log(dataSource.findProductById(productId));
+const productDetails = new ProductDetails(productId, dataSource);
+productDetails.init();
 
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  productDetails.addProductToCart(product);
 }
 
 // add listener to Add to Cart button
