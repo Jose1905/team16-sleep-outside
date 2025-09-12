945c8c5b8e42e0a530e93b53a944075f34b258a8
jkjorgie <jkjorgie@yahoo.com>
2025-09-11 18:33:24 -0600

    ran lint, format, build

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,11 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const productList = new ProductList(dataSource.category, dataSource, document.querySelector(".product-list"));
+const productList = new ProductList(
+  dataSource.category,
+  dataSource,
+  document.querySelector(".product-list"),
+);
 productList.init();
 
 // Update cart count on page load
